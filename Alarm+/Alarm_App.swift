543330b3a00e15755a67ec77f9a4d5d738efaca5
rpatil5 <rishistar05@gmail.com>
2026-07-22 22:58:24 -0500

    Initial Commit

---
+++ Alarm+/Alarm_App.swift
@@ -0,0 +1,17 @@
+//
+//  Alarm_App.swift
+//  Alarm+
+//
+//  Created by Rishit Patil on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Alarm_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
